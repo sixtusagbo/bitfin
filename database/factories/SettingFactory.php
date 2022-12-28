373e9286cf9c00f619a64fd5279e127516ee4ebb
sixtusagbo <miracleagbosixtus@gmail.com>
2022-12-28 01:56:31 +0100

    [Feature] Make whole app dynamic
    By fetching settings from db

---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Setting>
+ */
+class SettingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'app_name' => 'Bitcoin Financial Company',
+            'email' => 'support@bitfin.co',
+            'youtube_video' => 'https://www.youtube.com/embed/WrgwWDszPMs?autoplay=1',
+            'currency' => '$',
+            'referral_worth' => 50,
+            'withdrawal_charge' => 0,
+            'min_investment' => 100,
+            'min_withdrawal' => 20,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PaymentWalletSeeder::class,
             PlanSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
