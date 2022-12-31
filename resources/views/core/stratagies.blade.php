af5eb96e2caee0d4d94ee4e61d14e84ed1bae957
sixtusagbo <miracleagbosixtus@gmail.com>
2023-01-01 00:53:22 +0100

    [Update] Strategy page

--- resources/views/core/stratagies.blade.php
+++ resources/views/core/stratagies.blade.php
@@ -1,32 +1,12 @@
 @extends('layouts.core')
 
 @section('content')
-    <section class="page-header hdr-company">
+    <section class="about mt-125">
         <div class="container">
-            <div class="page-title float-left d-inline-block">
-                <h1>Strategies</h1>
-                <ul>
-                    <li><a href="{{ url('/') }}">Home</a></li>
-                    <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-chevron-right feather-icon">
-                            <polyline points="9 18 15 12 9 6"></polyline>
-                        </svg></li>
-                    <li>Company</li>
-                    <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-chevron-right feather-icon">
-                            <polyline points="9 18 15 12 9 6"></polyline>
-                        </svg></li>
-                    <li>Strategies</li>
-                </ul>
+            <div class="section-header">
+                <p>Strategies</p>
+                <h2>Our Strategies</h2>
             </div>
-            <div class="clearfix d-block d-sm-none"></div>
-        </div>
-    </section>
-
-    <section class="section-sp">
-        <div class="container wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
             <div class="strat">
                 <h1>Hedge Fund Strategy And Private Equity</h1>
                 <p>{{ config('myglobals.app_name') }} has invested and generated some amazing compounded annual returns by
