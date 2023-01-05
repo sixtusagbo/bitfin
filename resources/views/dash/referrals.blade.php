d17959ce53be16fe4eb38725321049fb0fa71e08
sixtusagbo <miracleagbosixtus@gmail.com>
2023-01-05 02:39:46 +0100

    [Update] Referrals UI

--- resources/views/dash/referrals.blade.php
+++ resources/views/dash/referrals.blade.php
@@ -1,36 +1,64 @@
 @extends('layouts.dash')
 
 @section('content')
-    <div class="bannerwrap insideheaders">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="bounceInDown wow">Your <span>Referrals</span></h1>
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1 class="app-page-title">Referrals</h1>
 
-    <div class="accwrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    @include('inc.accountnav')
-                </div>
-                <div class="col-lg-9">
-                    <div class="member-container">
-
-                        <div class="acchead">
-                            <div class="left">Total Referrals
-                                <strong>{{ Auth::user()->referrals->count() }}(Active)</strong>
-                            </div>
-                            <div class="right">Earned Commission <strong>@money(Auth::user()->referrals->count() * 10)</strong></div>
+    <div class="row g-4 mb-4">
+        <div class="col-12 col-lg-12">
+            <div class="app-card h-100 shadow-sm">
+                <div class="app-card-header p-3">
+                    <div class="row justify-content-between align-items-center">
+                        <div class="col-auto">
+                            <h4 class="app-card-title">
+                                Total Referrals
+                                ({{ Auth::user()->referrals->count() }})
+                            </h4>
                         </div>
+                        <!--//col-->
+                        <div class="col-auto">
+                            <h4 class="app-card-title">
+                                Earned Commission
+                                (@money(Auth::user()->referrals->count() * 10))
+                            </h4>
+                        </div>
+                        <!--//col-->
+                    </div>
+                    <!--//row-->
+                </div>
+                <!--//app-card-header-->
+                <div class="app-card-body p-3 p-lg-4">
+                    <div class="table-responsive">
+                        <table class="table table-striped mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="meta">Name</th>
+                                    <th class="meta stat-cell">Email Address</th>
+                                    <th class="meta stat-cell">Referred on</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse (Auth::user()->referrals as $user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->created_at->toDayDateTimeString() }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="4" class="bg-white">
+                                            <div class="alert alert-info text-center" role="alert">No referrals
+                                                yet</div>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+                <!--//app-card-body-->
+
             </div>
         </div>
     </div>
-    <div class="strip">&nbsp;</div>
-    <div class="strip">&nbsp;</div>
+    <!--//row-->
 @endsection
--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -88,7 +88,7 @@
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
                                     <li><a class="dropdown-item" href="{{ url('/deposit') }}">Deposit Now</a></li>
-                                    <li><a class="dropdown-item" href="{{ url('/edit_account') }}">Settings</a></li>
+                                    <li><a class="dropdown-item" href="{{ url('/edit_account') }}">Profile</a></li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
@@ -228,6 +228,23 @@
                                     </svg>
                                 </span>
                                 <span class="nav-link-text">Withdraw</span>
+                            </a>
+                            <!--//nav-link-->
+                        </li>
+
+                        <!--//nav-item-->
+                        <li class="nav-item">
+                            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+                            <a class="nav-link @if (Request::is('profile')) active @endif"
+                                href="{{ route('profile') }}">
+                                <span class="nav-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+                                    </svg>
+                                </span>
+                                <span class="nav-link-text">Profile</span>
                             </a>
                             <!--//nav-link-->
                         </li>
